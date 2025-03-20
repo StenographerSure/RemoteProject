d17ca3d0b025587f1a4e5d307fa8d320cb77368a
StenographerSure <glibockiy@inbox.ru>
2025-03-20 21:21:02 +0400

    Done sets HW

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -16,17 +16,22 @@ public class App {
         Product apple = new SimpleProduct("apple", 5);
         Product banana = new DiscountedProduct("Banana", 20, 35);
         Product sausage = new DiscountedProduct("Sausage", 50, 5);
-        searchEngine.add(apple);
-        searchEngine.add(banana);
-        searchEngine.add(sausage);
-        searchEngine.add(new SimpleProduct("applesauce", 15));
-        searchEngine.add(new SimpleProduct("applejam", 15));
-        searchEngine.add(new Article("A New app", "delicious applications beginning"));
-        searchEngine.add(new Article("New app", "appappapp"));
 
         productBasket.addProduct(apple);
         productBasket.addProduct(banana);
         productBasket.addProduct(sausage);
+
+        searchEngine.add(apple);
+        searchEngine.add(banana);
+        searchEngine.add(sausage);
+
+        searchEngine.add(new SimpleProduct("applesauce", 15));
+        searchEngine.add(new SimpleProduct("applejam", 15));
+
+        searchEngine.add(new Article("A New app", "delicious applications beginning"));
+        searchEngine.add(new Article("New app", "appappapp"));
+
+
 
 
         //demo
@@ -47,31 +52,6 @@ public class App {
         System.out.println(searchEngine.search("app"));
 
 
-        //demo 2
-
-//        try {
-//            Product product = new SimpleProduct("new", 0);
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//
-//        try {
-//            Product product = new DiscountedProduct("   ", 0, 105);
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//
-//        try {
-//            System.out.println(searchEngine.searchPrecise("app"));
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//
-//        try {
-//            System.out.println(searchEngine.searchPrecise("JOB"));
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
 
     }
 }
